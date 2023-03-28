c44fc298f3ab3e9bac942a33da95ee64ef91d234
BonggonPark <devparkbonggon@gmail.comgit config --global user.email devparkbonggon@gmail.com>
2023-03-28 12:17:32 +0900

    추가

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import styled from 'styled-components';
+
+const Maybe = styled.div`
+  background-color: #ddd;
+  width:  1000px;
+  height: 500px;
+`;
+
+function App() {
+  return (
+    <Maybe>
+      ks
+    </Maybe>
+  );
+}
+
+
+
+export default App;
